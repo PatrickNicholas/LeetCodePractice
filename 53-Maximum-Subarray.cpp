88ba26063d57efedd35c018fe722ae3326b73774
Maochuan <thinkermao@outlook.com>
2017-03-25 10:12:18 +0800

    53'

---
+++ 53-Maximum-Subarray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int res = INT_MIN, sum = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            sum += nums[i];
+            res = max(sum, res);
+            if (sum < 0) {
+                sum = 0;
+                continue;
+            }
+        }
+        return res;
+    }
+};
